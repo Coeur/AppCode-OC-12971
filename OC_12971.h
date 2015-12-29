9c641be440e6e8116db409be815aebfadd570436
Antoine Cœur <coeur@gmx.fr>
2015-12-29 21:10:11 +0800

    Not implemented method is implemented

---
+++ OC_12971.h
@@ -0,0 +1,17 @@
+//
+//  OC_12971.h
+//  OC-12971
+//
+//  Created by Antoine Cœur on 29/12/2015.
+//  Copyright © 2015 Antoine Cœur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OC_12971 : NSObject
+
+#if DEBUG
++ (void)foobar;
+#endif
+
+@end
---
+++ OC_12971.m
@@ -0,0 +1,17 @@
+//
+//  OC_12971.m
+//  OC-12971
+//
+//  Created by Antoine Cœur on 29/12/2015.
+//  Copyright © 2015 Antoine Cœur. All rights reserved.
+//
+
+#import "OC_12971.h"
+
+@implementation OC_12971
+
+#if DEBUG
++ (void)foobar {}
+#endif
+
+@end
